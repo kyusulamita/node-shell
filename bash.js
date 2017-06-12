a51dd8a214ca80baa10942595627d0ef46654392
kyusulamita <sulamitamorales@yahoo.com>
2017-06-12 10:53:12 -0400

    adds modules

--- bash.js
+++ bash.js
@@ -1,17 +1,23 @@
+var commands = require('./commands');
+
+
 process.stdout.write("my_prompt > ");
 process.stdin.on('data', function(data){
 	var cmd = data.toString().trim();
 	if(cmd === 'pwd'){
-		console.log(process.execPath);
+		// console.log(process.execPath);
+		commands[pwd]();
 	}
 	else if(cmd === 'date'){
 		var d = new Date();
 		console.log(d.toString());
+		process.stdout.write("my_prompt > ");
 	}
 	else if(cmd === "Q" || cmd === "quit"){
 		// exit ;
+		process.stdout.write("my_prompt > ");
 	}
-	process.stdout.write("my_prompt > ");
+
 });
 // // input prompt pwd
 // console.log(process.execPath);
---
+++ commands.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+	pwd : function(){
+		console.log(process.execPath);
+		process.stdout.write("my_prompt > ");		
+	}
+	date : function(){
+		
+	}
+}
