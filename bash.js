cf004b5a9c7d154eeb9bd936160b3cdaa8316601
kyusulamita <sulamitamorales@yahoo.com>
2017-06-12 10:56:36 -0400

    adds modules correctly

--- bash.js
+++ bash.js
@@ -1,27 +1,6 @@
 var commands = require('./commands');
-
-
 process.stdout.write("my_prompt > ");
 process.stdin.on('data', function(data){
 	var cmd = data.toString().trim();
-	if(cmd === 'pwd'){
-		// console.log(process.execPath);
-		commands[pwd]();
-	}
-	else if(cmd === 'date'){
-		var d = new Date();
-		console.log(d.toString());
-		process.stdout.write("my_prompt > ");
-	}
-	else if(cmd === "Q" || cmd === "quit"){
-		// exit ;
-		process.stdout.write("my_prompt > ");
-	}
-
+	commands[cmd]();
 });
-// // input prompt pwd
-// console.log(process.execPath);
-// //input prompt date
-// var d = new Date();
-// var a = d.toString();
-// console.log(d);
--- commands.js
+++ commands.js
@@ -3,8 +3,10 @@ module.exports = {
 	pwd : function(){
 		console.log(process.execPath);
 		process.stdout.write("my_prompt > ");		
-	}
+	},
 	date : function(){
-		
+		var d = new Date();
+		console.log(d.toString());
+		process.stdout.write("my_prompt > ");
 	}
 }
