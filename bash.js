0806fa645887f197be4dd7cf7bf30ab711d4355b
kyusulamita <sulamitamorales@yahoo.com>
2017-06-12 12:09:30 -0400

    head, tail, sorts

--- bash.js
+++ bash.js
@@ -1,10 +1,11 @@
 var commands = require('./commands');
-process.stdout.write("my_prompt > ");
+process.stdout.write("_prompt > ");
 process.stdin.on('data', function(data){
-	var cmd = data.toString().trim();
-	if(cmd.split(' ').length > 1){
-		cmd = cmd.split(' ');
-		commands[cmd[0]](cmd);
+	var cmd = data.toString().trim().split(' ');
+	var param = null;
+	if(cmd.length > 1){
+		param = cmd.slice(1);
+		cmd = cmd[0];
 	}
-	commands[cmd]();
+	commands[cmd](param);
 });
--- commands.js
+++ commands.js
@@ -2,12 +2,12 @@ var fs = require('fs');
 module.exports = {
 	pwd : function(){
 		console.log(process.execPath);
-		process.stdout.write("my_prompt > ");		
+		process.stdout.write("Best_team_prompt > ");		
 	},
 	date : function(){
 		var d = new Date();
 		console.log(d.toString());
-		process.stdout.write("my_prompt > ");
+		process.stdout.write("Best_team_prompt > ");
 	},
 	ls : function(){
 		fs.readdir('.', function(err, files){
@@ -15,12 +15,42 @@ module.exports = {
 			files.forEach(function(file){
 				process.stdout.write(file.toString() + "\n");
 			});
+			process.stdout.write("Best_team_prompt > ");
 		});
 
 	},
-	echo : function(cmd){
-		cmd.shift();
-		console.log(cmd.join(' '));
-		process.stdout.write("my_prompt > ");
+	echo : function(){
+		// to_echo.shift();
+		console.log(to_echo.join(' '));
+		process.stdout.write("Best_team_prompt > ");
+	},
+	cat : function(param){
+		// param = param.join(' ');
+		fs.readFile(param[0], 'utf8',(err, data) => {
+		  	if (err) throw err;
+ 		  	console.log(data);
+ 			process.stdout.write("Best_team_prompt > ");
+		});	
+	},
+	head : function(param){
+		fs.readFile(param[0], 'utf8',(err, data) => {
+		  	if (err) throw err;
+ 			var data  = data.split('\n').slice(0, 5).join('\n');
+ 			console.log(data);
+ 			process.stdout.write("Best_team_prompt > ");
+		});
+	},
+	tail : function(param){
+		fs.readFile(param[0], 'utf8',(err, data) => {
+		  if (err) throw err;
+ 			var data  = data.split('\n').slice(-5).join('\n');
+ 			console.log(data);
+ 			process.stdout.write("Best_team_prompt > ");
+		});
+	},
+	sort : function(param){
+		fs.readFile(param[0], 'utf8', (err, data) =>{
+			if(err) throw err;
+		});
 	}
 }
