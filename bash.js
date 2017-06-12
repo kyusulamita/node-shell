64401d713289b09cb9c29e1bf4a3e18e3b0b6516
kyusulamita <sulamitamorales@yahoo.com>
2017-06-12 13:08:00 -0400

    implements todo requested

--- bash.js
+++ bash.js
@@ -2,10 +2,7 @@ var commands = require('./commands');
 process.stdout.write("Best_team_prompt > ");
 process.stdin.on('data', function(data){
 	var cmd = data.toString().trim().split(' ');
-	var param = null;
-	if(cmd.length > 1){
-		param = cmd.slice(1);
-		cmd = cmd[0];
-	}
+	var param = (cmd.length > 1) ? cmd.slice(1) : null;
+	cmd = cmd[0];
 	commands[cmd](param);
 });
--- commands.js
+++ commands.js
@@ -6,15 +6,11 @@ var done = function(output){
 }
 module.exports = {
 	pwd : function(){
-		// console.log(process.execPath);
-		// process.stdout.write("Best_team_prompt > ");	
 		done(process.execPath);	
 	},
 	date : function(){
 		var d = new Date();
 		done(d.toString());
-		// console.log(d.toString());
-		// process.stdout.write("Best_team_prompt > ");
 	},
 	ls : function(){
 		var output = "";
@@ -23,23 +19,17 @@ module.exports = {
 			files.forEach(function(file){
 				output += file.toString() + "\n";
 			});
-			// process.stdout.write("Best_team_prompt > ");
 			done(output);
 		});
 
 	},
 	echo : function(param){
 		done(param.join(' '));
-		// console.log(param.join(' '));
-		// process.stdout.write("Best_team_prompt > ");
 	},
 	cat : function(param){
-		// param = param.join(' ');
 		fs.readFile(param[0], 'utf8',(err, data) => {
 		  	if (err) throw err;
 		  	done(data);
- 		 //  	console.log(data);
- 			// process.stdout.write("Best_team_prompt > ");
 		});	
 	},
 	head : function(param){
@@ -47,8 +37,6 @@ module.exports = {
 		  	if (err) throw err;
  			var data  = data.split('\n').slice(0, 5).join('\n');
  			done(data);
- 			// console.log(data);
- 			// process.stdout.write("Best_team_prompt > ");
 		});
 	},
 	tail : function(param){
@@ -56,8 +44,6 @@ module.exports = {
 		  if (err) throw err;
  			var data  = data.split('\n').slice(-5).join('\n');
  			done(data);
- 			// console.log(data);
- 			// process.stdout.write("Best_team_prompt > ");
 		});
 	},
 	sort : function(param){
@@ -65,16 +51,12 @@ module.exports = {
 			if(err) throw err;
 			var data = data.split('\n').sort().join('\n');
 			done(data);
-			// console.log(data);
-			// process.stdout.write("Best_team_prompt > ");
 		});
 	},
 	wc : function(param){
 		fs.readFile(param[0], 'utf8', (err, data) =>{
 			if(err) throw err;
 			done(data.split('\n').length);
-			// console.log(data.split('\n').length);
-			// process.stdout.write("Best_team_prompt > ");		
 		});
 	},
 	uniq : function(param){
@@ -86,18 +68,13 @@ module.exports = {
 			}
 			data = data.split('\n').filter(noDupe).join('\n');
 			done(data);
-			// console.log(data);
-			// process.stdout.write("Best_team_prompt > ");	
 		});
 	},
 	curl : function(param){
 		var url = "http://www." + param[0];
-		// url += param[0]
 		request(url, (error, response, body) =>{
 			if(error) console.log(error);
-			done(response.body);
-			// if(response) console.log(response.body);
-			// process.stdout.write("Best_team_prompt > ");				
+			done(response.body);			
 		})
 	}
 }
